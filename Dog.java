a257a691e46574133142856fde6f6535780029c5
The_Pi$ith <pisith@Pisiths-MacBook-Pro.local>
2024-04-03 10:32:56 +0700

    implemented sample OOP Encapsulation

---
+++ Dog.java
@@ -0,0 +1,34 @@
+class GoodDog {
+    // Stat
+    private int size;
+
+    // Behaviors
+    public int getSize() {
+        return size;
+    }
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public void bark() {
+        if (size > 60) System.out.println("Woof, Woof!!");
+        else if (size > 14) System.out.println("Ruff, Ruff!!");
+        else System.out.println("Yip!, Yip!");
+    }
+}
+
+class Dog {
+    public static void main(String[] args) {
+        GoodDog dog1 = new GoodDog();
+        dog1.setSize(70);
+
+        GoodDog dog2 = new GoodDog();
+        dog2.setSize(25);
+
+        System.out.println("Dog1 size : " + dog1.getSize());
+        System.out.println("Dog2 size : " + dog2.getSize());
+
+        dog1.bark();
+        dog2.bark();
+    }
+}
